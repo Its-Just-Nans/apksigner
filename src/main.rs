4b8909cfd9d24441dbd28ad93b7417390fe0aa2d
n4n5 <its.just.n4n5@gmail.com>
2025-02-11 13:43:11 +0100

    update main

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
 fn main() {
-    println!("Nothing for now - see https://docs.rs/apksig");
+    println!("This is not apksigner from https://developer.android.com/tools/apksigner");
+    println!("This is the apksigner from https://docs.rs/apksigner");
+    println!("Nothing for now - maybe a CLI for later");
+    println!("See also https://docs.rs/apksig");
 }
